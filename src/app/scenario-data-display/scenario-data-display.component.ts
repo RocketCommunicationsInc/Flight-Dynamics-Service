1e8da30ff20f300d3cd074dc36c3367cc6974389
Maria Paterno <97068764+Paterma@users.noreply.github.com>
2023-09-11 11:10:57 -0600

    feature: creating scenario data display component

--- src/app/scenario-data-display/scenario-data-display.component.ts
+++ src/app/scenario-data-display/scenario-data-display.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { AstroComponentsModule } from '@astrouxds/angular'
 
 @Component({
   standalone: true,
   selector: 'app-scenario-data-display',
   templateUrl: './scenario-data-display.component.html',
   styleUrls: ['./scenario-data-display.component.css'],
+  imports: [AstroComponentsModule]
 })
-export class ScenarioDataDisplayComponent {}
+export class ScenarioDataDisplayComponent {
+  isDialogOpen: boolean = false;
+
+  showDialog() {
+    this.isDialogOpen = true;
+  }
+}
