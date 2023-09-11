362071ec722d73419129e5bbfdb63d8e654ec177
Maria Paterno <97068764+Paterma@users.noreply.github.com>
2023-09-11 11:20:20 -0600

    chore: updating grid scaffolding to reflect containers in app wireframe, not app anatomy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,18 @@
 import { Component } from '@angular/core';
-import { UtilityToolkitComponent } from './utility-toolkit/utility-toolkit.component';
 import { ScenarioDataDisplayComponent } from './scenario-data-display/scenario-data-display.component';
 import { ScenarioLibraryComponent } from './scenario-library/scenario-library.component';
 import { GlobalStatusBarComponent } from './global-status-bar/global-status-bar.component';
-import { OutputDataDisplayComponent } from './output-data-display/output-data-display.component';
-import { DataInputsOutputsComponent } from './data-inputs-outputs/data-inputs-outputs.component';
-
+import { MainComponent } from './main/main.component';
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   imports: [
-    UtilityToolkitComponent,
     ScenarioDataDisplayComponent,
     ScenarioLibraryComponent,
     GlobalStatusBarComponent,
-    OutputDataDisplayComponent,
-    DataInputsOutputsComponent,
+    MainComponent,
   ],
 })
 export class AppComponent {
