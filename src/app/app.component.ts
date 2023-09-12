8b515b51cd0e4569abe44c4e5d6dda6bc563c846
Jacob Norton <jacob.norton@rocketcom.com>
2023-09-12 18:19:15 -0400

    refactoring global status bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ScenarioDataDisplayComponent } from './scenario-data-display/scenario-data-display.component';
 import { ScenarioLibraryComponent } from './scenario-library/scenario-library.component';
 import { GlobalStatusBarComponent } from './global-status-bar/global-status-bar.component';
 import { MainComponent } from './main/main.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { AstroComponentsModule, RuxToastStack } from '@astrouxds/angular';
+import { ToastConfig, ToastService } from './shared/toast.service';
+import { BehaviorSubject, Subject, filter, takeUntil, tap } from 'rxjs';
 
 @Component({
   standalone: true,
@@ -17,8 +20,31 @@ import { RouterLink, RouterOutlet } from '@angular/router';
     MainComponent,
     RouterLink,
     RouterOutlet,
+    AstroComponentsModule
   ],
 })
-export class AppComponent {
-  title = 'flight-dynamics';
+export class AppComponent implements OnInit, OnDestroy {
+  @HostBinding('class.light-theme') lightTheme: boolean = false;
+  @ViewChild(RuxToastStack) toastStack?: HTMLRuxToastStackElement | null;
+  destroyed = new Subject(); // Cleans up subscriptions to avoid memory leaks
+
+  changeTheme() {
+    this.lightTheme = !this.lightTheme;
+  }
+
+  constructor(private toasts: ToastService) { }
+
+  ngOnInit() {
+    this.toasts.getStack().pipe(
+      takeUntil(this.destroyed),
+      filter((val): val is ToastConfig => !!val)
+    ).subscribe((config: ToastConfig) =>
+      this.toastStack?.addToast(config)
+  );
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed.next(true);
+    this.destroyed.complete();
+  }
 }
