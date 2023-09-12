97fc33e2b6305b54d03f16add0b47e449c9dc134
Jacob Norton <jacob.norton@rocketcom.com>
2023-09-12 13:11:42 -0400

    Switched 'app' selector to 'fds'

--- src/app/scenario-data-display/scenario-data-display.component.ts
+++ src/app/scenario-data-display/scenario-data-display.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
-  selector: 'app-scenario-data-display',
+  selector: 'fds-scenario-data-display',
   templateUrl: './scenario-data-display.component.html',
   styleUrls: ['./scenario-data-display.component.css'],
 })
--- src/app/scenario-library/scenario-library.component.ts
+++ src/app/scenario-library/scenario-library.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
-  selector: 'app-scenario-library',
+  selector: 'fds-scenario-library',
   templateUrl: './scenario-library.component.html',
   styleUrls: ['./scenario-library.component.css'],
 })
