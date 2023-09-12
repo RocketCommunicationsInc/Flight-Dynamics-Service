5278455a634750243a4502a23f21b966927b1e61
Brian <brian@briankiernan.com>
2023-09-12 16:09:54 -0400

    merge

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { AstroComponentsModule } from '@astrouxds/angular';
 
 @Component({
   selector: 'fds-main',
   standalone: true,
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css'],
+  imports: [AstroComponentsModule],
 })
 export class MainComponent {}
