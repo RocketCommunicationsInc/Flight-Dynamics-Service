63199bbf954667686f3eaa4308a4cc35fab2708c
Kiley Mitti <kiley.mitti@rocketcom.com>
2023-09-12 11:29:33 -0400

    basic html setup

--- src/app/global-status-bar/global-status-bar.component.ts
+++ src/app/global-status-bar/global-status-bar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { AstroComponentsModule } from '@astrouxds/angular';
 
 @Component({
   standalone: true,
   selector: 'app-global-status-bar',
   templateUrl: './global-status-bar.component.html',
   styleUrls: ['./global-status-bar.component.css'],
+  imports: [AstroComponentsModule],
 })
 export class GlobalStatusBarComponent {}
