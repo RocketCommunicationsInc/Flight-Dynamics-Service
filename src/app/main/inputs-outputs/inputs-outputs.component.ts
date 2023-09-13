1ae11f0a7dff296fb2eada895958b648e4e2a986
Frances Morrison <francesrocket@Rocket-FRANCES.M>
2023-09-13 10:51:48 -0400

    feat(inputs-outputs): adding tab structure

--- src/app/main/inputs-outputs/inputs-outputs.component.ts
+++ src/app/main/inputs-outputs/inputs-outputs.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChildContainerComponent } from 'src/app/shared/child-container/child-container.component';
+import { AstroComponentsModule } from '@astrouxds/angular';
 
 @Component({
   selector: 'fds-inputs-outputs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AstroComponentsModule, ChildContainerComponent],
   templateUrl: './inputs-outputs.component.html',
-  styleUrls: ['./inputs-outputs.component.css']
+  styleUrls: ['./inputs-outputs.component.css'],
 })
-export class InputsOutputsComponent {
-
-}
+export class InputsOutputsComponent {}
