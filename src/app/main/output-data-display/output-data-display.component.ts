163ef07f49ceb3c5112e1b8d5c5cf4a208be2bf0
Brian <brian@briankiernan.com>
2023-09-13 20:36:23 +0000

    add diviations

--- src/app/main/output-data-display/output-data-display.component.ts
+++ src/app/main/output-data-display/output-data-display.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AstroComponentsModule } from '@astrouxds/angular';
+import {
+  SegmentedButton,
+  Status,
+} from '@astrouxds/astro-web-components/dist/types/components';
 
 import { ChildContainerComponent } from 'src/app/shared';
+
+function random(min = 10_000, max = 80_000) {
+  return Math.random() * (max - min) + min;
+}
+
+interface OrbitDeterminations {
+  message: string;
+  status: Status;
+  timestamp: number;
+}
 
 @Component({
   selector: 'fds-output-data-display',
@@ -12,26 +26,69 @@ import { ChildContainerComponent } from 'src/app/shared';
   styleUrls: ['./output-data-display.component.css'],
 })
 export class OutputDataDisplayComponent {
+  now = new Date().getTime();
+  criticals: OrbitDeterminations[] = [];
+  diviations: OrbitDeterminations[] = [];
+  warnings: OrbitDeterminations[] = [];
+
   tabs = [
     { label: 'OD Solution', selected: true },
     { label: 'OD Performance', disabled: true },
   ];
 
-  notifications = [
+  ods: OrbitDeterminations[] = [
     {
-      message: 'Happened before the notification at index 1',
-      status: 'critical',
-      timestamp: new Date().getTime() - 10_000,
+      message: 'OD warning message on',
+      status: 'caution',
+      timestamp: this.now - random(),
     },
     {
-      message: 'OD succesfully completed on ',
+      message: 'OD critical message on',
+      status: 'critical',
+      timestamp: this.now - random(),
+    },
+    {
+      message: 'OD success message on',
       status: 'normal',
-      timestamp: new Date().getTime(),
+      timestamp: this.now,
+    },
+    {
+      message: 'OD success message on',
+      status: 'normal',
+      timestamp: this.now - random(),
+    },
+    {
+      message: 'OD warning message on',
+      status: 'caution',
+      timestamp: this.now - random(),
+    },
+    {
+      message: 'OD warning message on',
+      status: 'caution',
+      timestamp: this.now - random(),
+    },
+    {
+      message: 'OD warning message on',
+      status: 'caution',
+      timestamp: this.now - random(),
+    },
+    {
+      message: 'OD success message on',
+      status: 'normal',
+      timestamp: this.now - random(),
     },
   ];
 
+  actions: SegmentedButton[] = [
+    { label: 'Secondary Action', selected: false },
+    { label: 'Primary Action', selected: true },
+  ];
+
   constructor() {
-    this.notifications.sort((a, b) => b.timestamp - a.timestamp);
+    this.criticals = this.ods.filter((od) => od.status === 'critical');
+    this.diviations = this.ods.filter((od) => od.status !== 'normal');
+    this.warnings = this.ods.filter((od) => od.status === 'caution');
+    this.ods.sort((a, b) => b.timestamp - a.timestamp);
   }
 
   onSelect(e: Event) {
