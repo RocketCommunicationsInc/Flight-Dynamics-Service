4e7772b1be9e17e759e490016fd173db34913611
Brian <brian@briankiernan.com>
2023-09-13 18:33:49 +0000

    add tabs

--- src/app/main/output-data-display/output-data-display.component.ts
+++ src/app/main/output-data-display/output-data-display.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AstroComponentsModule, RuxTab } from '@astrouxds/angular';
+
 import { ChildContainerComponent } from 'src/app/shared';
+
+interface Tab {
+  label: string;
+  disabled?: RuxTab['disabled'];
+  selected?: RuxTab['selected'];
+}
 
 @Component({
   selector: 'fds-output-data-display',
   standalone: true,
-  imports: [CommonModule, ChildContainerComponent],
+  imports: [CommonModule, ChildContainerComponent, AstroComponentsModule],
   templateUrl: './output-data-display.component.html',
   styleUrls: ['./output-data-display.component.css'],
 })
-export class OutputDataDisplayComponent {}
+export class OutputDataDisplayComponent {
+  tabs: Tab[] = [
+    { label: 'OD Solution', selected: true },
+    { label: 'OD Performance', disabled: true },
+  ];
+
+  onSelect(e: Event) {
+    console.log(e);
+  }
+}
