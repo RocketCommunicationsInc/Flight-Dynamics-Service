312c2de60f31e3817541a5475a0aa75bdf2e5039
Maria Paterno <97068764+Paterma@users.noreply.github.com>
2023-09-15 10:02:02 -0600

    feature: checkboxes disable and enable based on length and save button is disabled until you have selected 6

--- src/app/properties-dialog/properties-dialog.component.ts
+++ src/app/properties-dialog/properties-dialog.component.ts
@@ -28,95 +28,46 @@ export class PropertiesDialogComponent {
       'Semi-Major Axis',
     ];
   }
+
   ngOnInit() {
     this.dialogService.getSelectedProperties().subscribe((checkedItems) => {
       this.dummyOptions.forEach((cb) => {
         cb.checked = checkedItems.includes(cb.cb);
+        if (!cb.checked) {
+          cb.disabled = !checkedItems.includes(cb.cb);
+        }
       });
     });
   }
 
-  // disableCheckboxes(checkbox: HTMLRuxCheckboxElement) {
-  //   if(this.checkedProperties === 6) {
-  //     if(!checkbox.checked) {
-  //       checkbox.disabled = true
-  //     } else {
-  //       checkbox.disabled = false
-  //     }
-  //   }
-  //  else {checkbox.disabled = false
-  // }
-  //   // return (
-  //   //    this.checkedProperties >= 6 && !checkbox.checked
-  //   // )
-  // }
-
-  disabledCheckboxes: boolean[] = this.dummyOptions.map((cb) => !cb.checked);
-
   checkedProperties: number = 6;
 
   updateDisabledValue() {
-    // const isDisabled = this.checkedProperties >= 6 ? false : true
-    // this.dummyOptions.forEach((cb) => {
-    //   if(!cb.checked) {
-    //     cb.disabled = isDisabled
-    //   }
-    // })
-    // const uncheckedCheckboxes = this.dummyOptions.filter((cb) => !cb.checked)
-    // this.dummyOptions.forEach((cb) => {
-    //   if(this.checkedProperties >= 6 || cb.checked) {
-    //     cb.disabled = false
-    //   } else {
-    //     cb.disabled = true
-    //   }
-    // })
-
-    // if(this.checkedProperties < 6) {
-    //   this.dummyOptions.forEach((cb, index) => {
-    //     if(!cb.checked) {
-    //       this.disabledCheckboxes[index] = true
-    //     }
-    //   })
-    // } else {
-    //   this.disabledCheckboxes = this.dummyOptions.map((cb) => cb.checked)
-    // }
-
-    this.dummyOptions.forEach((property) => {
-      if (this.checkedProperties < 6) {
-        property.disabled = false;
-      } else if (!property.checked) {
-        property.disabled = true;
-      } else {
-        property.disabled = false;
-      }
+    const isDisabled = this.checkedProperties >= 6 ? true : false;
+    this.dummyOptions.forEach((cb) => {
+      const isChecked = this.updatedProperties.includes(cb.cb);
+      if (isChecked) {
+        cb.disabled = false;
+      } else cb.disabled = isDisabled;
     });
   }
 
   onCheckboxSelection(event: any) {
     const checkbox = event.target as HTMLRuxCheckboxElement;
-    const indexVal = this.updatedProperties.indexOf(
-      (cb: any) => cb.label === cb.value,
-    );
+
     if (checkbox.checked) {
       if (!this.updatedProperties.includes(checkbox.value)) {
         this.updatedProperties.push(checkbox.value);
         this.checkedProperties++;
-        // this.disabledCheckboxes[indexVal]= false
       }
     } else {
       this.checkedProperties--;
-      // this.disabledCheckboxes[indexVal]= true
       const index = this.updatedProperties.indexOf(checkbox.value);
-
       if (index !== -1) {
         this.updatedProperties.splice(index, 1);
       }
     }
-    this.checkedProperties = Math.max(this.checkedProperties, 0);
     this.updateDisabledValue();
-    // const isDisabled = this.disableCheckboxes(checkbox)
-    // checkbox.disabled = isDisabled
-    // this.disableCheckboxes(checkbox)
   }
 
   disableSave(): boolean {
