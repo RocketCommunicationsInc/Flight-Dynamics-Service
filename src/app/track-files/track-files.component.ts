bd9ebba18f793f6707c067eb06fdea837bf40e36
Maria Paterno <97068764+Paterma@users.noreply.github.com>
2023-09-18 13:27:33 -0600

    feature: adding track selection functionality

--- src/app/track-files/track-files.component.ts
+++ src/app/track-files/track-files.component.ts
@@ -8,8 +8,46 @@ import { dummyFileData } from './dummy-file-data';
   standalone: true,
   imports: [CommonModule, AstroComponentsModule],
   templateUrl: './track-files.component.html',
-  styleUrls: ['./track-files.component.css']
+  styleUrls: ['./track-files.component.css'],
 })
 export class TrackFilesComponent {
-dummyFileData = dummyFileData
+  dummyFileData = dummyFileData;
+
+  selectedFileName: string = '';
+  selectedFileContent: string = '';
+  editedContent: string = '';
+  editTrackFile: boolean = false;
+  isFileSelected: boolean = false;
+
+  handleSelect(file: any): void {
+    file.selected = !file.selected;
+    if (file.selected) {
+      this.selectedFileName = file.fileName;
+      this.selectedFileContent = file.content;
+      this.isFileSelected = true;
+    } else {
+      this.selectedFileName = '';
+      this.selectedFileContent = '';
+      this.isFileSelected = false;
+    }
+  }
+
+  handleClick() {
+    this.editTrackFile = true;
+  }
+
+  handleCancel() {
+    this.editTrackFile = false;
+  }
+
+  handleSave() {
+    this.selectedFileContent = this.editedContent;
+    this.editTrackFile = false;
+  }
+
+  handleTextarea(event: any) {
+    if (event.target.value) {
+      this.editedContent = event.target.value;
+    }
+  }
 }
