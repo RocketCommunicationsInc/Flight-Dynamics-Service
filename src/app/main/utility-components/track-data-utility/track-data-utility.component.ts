83933eea3a3465729bee997b7ff06e25cff8f05e
Maria Paterno <97068764+Paterma@users.noreply.github.com>
2023-09-19 17:22:13 -0600

    feature (DEMO-367 ): Create Select Files Tab (#26)
    * feature: creating select files tab
    
    * feature: adding track selection functionality
    
    * feature: adding in sort functionality to table, added filter functionaliy
    
    * feature: working on sort functionality
    
    * feature: adding sort functionality to table header
    
    * chore: adding last bit of logic for checkboxes
    
    * chore: remove duplicate logic
    
    * chore: updating logic to if no checkboxe are selected, don't show content to the right
    
    * chore: remove console.log

--- src/app/main/utility-components/track-data-utility/track-data-utility.component.ts
+++ src/app/main/utility-components/track-data-utility/track-data-utility.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { AstroComponentsModule } from '@astrouxds/angular';
+import { TrackFilesComponent } from 'src/app/track-files/track-files.component';
+import { TrackDataComponent } from 'src/app/track-data/track-data.component';
 @Component({
   selector: 'fds-track-data-util',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AstroComponentsModule, TrackFilesComponent, TrackDataComponent],
   templateUrl: './track-data-utility.component.html',
   styleUrls: ['./track-data-utility.component.css'],
 })
-export class TrackDataUtilityComponent {}
+export class TrackDataUtilityComponent {
+  selectedTab: string = 'files';
+
+  tabSelect(event: any) {
+    if (event.target.id) {
+      this.selectedTab = event.target.id;
+    }
+  }
+}
