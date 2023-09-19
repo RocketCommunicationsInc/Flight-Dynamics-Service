a43dd66f1558276e6e0602d1aa5a135f48bce2a8
Maria Paterno <97068764+Paterma@users.noreply.github.com>
2023-09-19 12:01:21 -0600

    chore: removing tabs and buttons to the right

--- src/app/main/utility-components/log-utility/log-utility.component.ts
+++ src/app/main/utility-components/log-utility/log-utility.component.ts
@@ -11,15 +11,7 @@ import { CommonModule } from '@angular/common';
   imports: [AstroComponentsModule, RouterLink, RouterOutlet, CommonModule],
 })
 export class LogUtilityComponent {
-  selectedTab: string = 'tab-a';
-
-  tabSelect(event: any) {
-    if (event.target.id) {
-      this.selectedTab = event.target.id;
-    }
-  }
-
-  logDataA = [
+  logData = [
     {
       timestamp: new Date(),
       status: 'off',
@@ -39,29 +31,6 @@ export class LogUtilityComponent {
       timestamp: new Date(),
       status: 'normal',
       message: 'Antenna DGS 4 went offline.',
-    },
-  ];
-
-  logDataB = [
-    {
-      timestamp: new Date(),
-      status: 'normal',
-      message: 'Antenna TCS 5 went offline.',
-    },
-    {
-      timestamp: new Date(),
-      status: 'caution',
-      message: 'Antenna TCS 6 went offline.',
-    },
-    {
-      timestamp: new Date(),
-      status: 'critical',
-      message: 'Antenna TCS 7 went offline.',
-    },
-    {
-      timestamp: new Date(),
-      status: 'normal',
-      message: 'Antenna TCS 8 went offline.',
     },
   ];
 }
