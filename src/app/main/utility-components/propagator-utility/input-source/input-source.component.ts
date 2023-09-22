f41ddad0d52e051170f46cf0bcfa697402cec299
Nic Dominguez <NicDominguez@users.noreply.github.com>
2023-09-22 10:43:08 -0700

    feat: adds controls drawer

--- src/app/main/utility-components/propagator-utility/input-source/input-source.component.ts
+++ src/app/main/utility-components/propagator-utility/input-source/input-source.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AstroComponentsModule } from '@astrouxds/angular';
+import { PropagatorControlsComponent } from '../propagator-controls/propagator-controls.component';
 @Component({
   selector: 'fds-input-source',
   standalone: true,
-  imports: [CommonModule, AstroComponentsModule],
+  imports: [CommonModule, AstroComponentsModule, PropagatorControlsComponent],
   templateUrl: './input-source.component.html',
   styleUrls: ['./input-source.component.css'],
 })
---
+++ src/app/main/utility-components/propagator-utility/propagator-controls/propagator-controls.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AstroComponentsModule } from '@astrouxds/angular';
+
+@Component({
+  selector: 'fds-propagator-controls',
+  standalone: true,
+  imports: [CommonModule, AstroComponentsModule],
+  templateUrl: './propagator-controls.component.html',
+  styleUrls: ['./propagator-controls.component.css'],
+})
+export class PropagatorControlsComponent {}
