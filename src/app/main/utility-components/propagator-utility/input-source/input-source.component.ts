77b19f129cf3aa47a7db14adc0fb7ac0e3ee87ee
Nic Dominguez <NicDominguez@users.noreply.github.com>
2023-09-22 13:20:37 -0700

    feat (DEMO-377): input source view (#35)
    * feat: adds template html
    
    * chore: set "semi" rule of prettierrc to true
    
    * feat: updating template
    
    * style: fix alignment issues
    
    * feat: adds controls drawer

--- src/app/main/utility-components/propagator-utility/input-source/input-source.component.ts
+++ src/app/main/utility-components/propagator-utility/input-source/input-source.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AstroComponentsModule } from '@astrouxds/angular';
+import { PropagatorControlsComponent } from '../propagator-controls/propagator-controls.component';
 @Component({
   selector: 'fds-input-source',
   standalone: true,
-  imports: [CommonModule, AstroComponentsModule],
+  imports: [CommonModule, AstroComponentsModule, PropagatorControlsComponent],
   templateUrl: './input-source.component.html',
-  styleUrls: ['./input-source.component.css']
+  styleUrls: ['./input-source.component.css'],
 })
 export class InputSourceComponent {
+  showControlsPanel: boolean = false;
 
+  toggleControls() {
+    this.showControlsPanel = !this.showControlsPanel;
+  }
 }
---
+++ src/app/main/utility-components/propagator-utility/propagator-controls/propagator-controls.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PropagatorControlsComponent } from './propagator-controls.component';
+
+describe('PropagatorControlsComponent', () => {
+  let component: PropagatorControlsComponent;
+  let fixture: ComponentFixture<PropagatorControlsComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [PropagatorControlsComponent]
+    });
+    fixture = TestBed.createComponent(PropagatorControlsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
