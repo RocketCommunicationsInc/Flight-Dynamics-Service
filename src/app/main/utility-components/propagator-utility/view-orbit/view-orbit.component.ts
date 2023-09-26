5a17f882194b60d268549fa7e5e8916f07a0a81c
Nic Dominguez <NicDominguez@users.noreply.github.com>
2023-09-26 08:24:40 -0700

    feat (DEMO-378 ): View Orbit tab in Propagator Utility (#38)
    * reafactor: separates into multiple components
    
    Co-authored-by: Maria Paterno <Paterma@users.noreply.github.com>
    
    * style: fixes map area flex grow
    
    ---------
    
    Co-authored-by: Maria Paterno <Paterma@users.noreply.github.com>

--- src/app/main/utility-components/propagator-utility/view-orbit/view-orbit.component.ts
+++ src/app/main/utility-components/propagator-utility/view-orbit/view-orbit.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core'
-import { CommonModule } from '@angular/common'
-import { AstroComponentsModule } from '@astrouxds/angular'
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AstroComponentsModule } from '@astrouxds/angular';
+import { PropagatorControlsComponent } from '../propagator-controls/propagator-controls.component';
+import { ViewTableComponent } from './view-table/view-table.component';
+import { ViewModelComponent } from './view-model/view-model.component';
 @Component({
   selector: 'fds-view-orbit',
   standalone: true,
-  imports: [CommonModule, AstroComponentsModule],
+  imports: [
+    CommonModule,
+    AstroComponentsModule,
+    PropagatorControlsComponent,
+    ViewTableComponent,
+    ViewModelComponent,
+  ],
   templateUrl: './view-orbit.component.html',
   styleUrls: ['./view-orbit.component.css'],
 })
-export class ViewOrbitComponent {}
+export class ViewOrbitComponent {
+  toggleControls() {
+    console.log('showControls');
+    this.showControlsPanel = !this.showControlsPanel;
+  }
+
+  showControlsPanel: boolean = false;
+  showModel: boolean = true;
+
+  viewTable() {
+    this.showModel = false;
+  }
+
+  viewModel() {
+    this.showModel = true;
+  }
+}
