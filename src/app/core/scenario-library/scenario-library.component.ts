c3aac49be22b80d7ec307fede8fd4d060e38adb7
Kiley Mitti <kiley.mitti@rocketcom.com>
2023-09-27 16:51:12 -0400

    some changes

--- src/app/core/scenario-library/scenario-library.component.ts
+++ src/app/core/scenario-library/scenario-library.component.ts
@@ -1,14 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal, ViewChild } from '@angular/core';
-import { AstroComponentsModule } from '@astrouxds/angular';
-import { select, State, Store } from '@ngrx/store';
-import { SpacecraftActions } from '../../+state/app.actions';
-import { selectScenarios } from '../../+state/app.reducer';
+import { Component} from '@angular/core';
+import { AstroComponentsModule, RuxTreeNode } from '@astrouxds/angular';
+import { State, Store } from '@ngrx/store';
+import { ScenariosActions, SpacecraftActions } from '../../+state/app.actions';
+import { selectScenarios, selectSelectedScenarioId, selectSelectedSpacecraft } from '../../+state/app.reducer';
 import { spacecraftSelector } from '../../+state/app.selectors';
 import { ToastService } from '../../shared/toast.service';
 import { Scenario, Spacecraft } from '../../types/data.types';
 import { Router } from '@angular/router';
-import { TrackFilesComponent } from 'src/app/main/utility-components/track-data-utility/track-files/track-files.component';
 import { AppStore, ScenariosState } from 'src/app/+state/app.model';
 import { Observable } from 'rxjs';
 @Component({
@@ -19,51 +18,63 @@ import { Observable } from 'rxjs';
   imports: [AstroComponentsModule, CommonModule],
 })
 export class ScenarioLibraryComponent {
-  selectedCraft = signal<string | null>('');
-  // scenarios$ = this.store.select(selectScenarios);
+  selectedSpacecraft$: Observable<Spacecraft | null>
+  selectedScenario$: Observable<string | null>
   scenarios$: Observable<ScenariosState>;
   spacecrafts$ = this.store.select(spacecraftSelector);
-  data: (Scenario | undefined)[] = [];
+  scenarios: (Scenario | undefined)[] = [];
   spacecraftData: any;
+  selectedSpacecraftId: string|null = null
+  selectedScenarioId: string|null = null
 
   constructor(
     private toasts: ToastService,
     private store: Store<AppStore>,
-    private state: State<AppStore>,
     private router: Router
   ) {
-    this.scenarios$ = this.store.pipe(select(selectScenarios));
+    this.scenarios$ = this.store.select(selectScenarios);
+    this.selectedSpacecraft$ = this.store.select(selectSelectedSpacecraft)
+    this.selectedScenario$ = this.store.select(selectSelectedScenarioId)
   }
 
-  /**
-   * Listen for the ruxtreenodeselected event and store the selected node in the selectedCraft signal
-   * @param el the rux-tree-node element
-   */
-  onTreeNodeSelected(e: Event) {
-    //!TODO This needs to be refactored to not access the DOM, ViewChild I think?
-    const targetID: string = (e.target as HTMLRuxTreeNodeElement).getAttribute(
-      'data-spacecraft'
-    )!;
-
-    this.spacecraftData.map((spacecraft: Spacecraft) => {
-      if (targetID === spacecraft.id) {
-        this.store.dispatch(
-          SpacecraftActions.spacecraftSelected({ spacecraft: spacecraft })
-        );
-      }
+  ngOnInit() {
+    this.scenarios$.subscribe((res: any) => {
+      this.scenarios = res.ids.map((id: string) => {
+        return res.entities[id];
+      });
     });
 
-    //!TODO This needs to be handled using state
-    const el = e.target as HTMLRuxTreeNodeElement;
-    const parentText = el.parentNode?.firstChild!.textContent?.trim();
-    //We don't want to select the parent nodes, just the nodes being used as slots
-    if (el.slot === 'node') {
-      this.selectedCraft.set(el.textContent);
-      this.router.navigateByUrl(
-        `${parentText}-${el.textContent?.trim()}` || ''
-      );
-    }
-    console.log(this.state);
+    this.selectedSpacecraft$.subscribe((res: Spacecraft|null) => {
+      this.selectedSpacecraftId = res?.id || null;
+    });
+    this.selectedScenario$.subscribe((res: string|null) => {
+      this.selectedScenarioId = res;
+    });
+
+    this.spacecrafts$.subscribe((res: any) => {
+      this.spacecraftData = res;
+    });
+
+    this.store.dispatch(
+      SpacecraftActions.spacecraftSelected({
+        spacecraft: this.spacecraftData[0],
+      })
+    );
+  }
+
+  onScenarioClick(event: Event) {
+    event.stopImmediatePropagation()
+    const scenario = event.target as HTMLRuxTreeNodeElement
+    scenario.setExpanded(true)
+  }
+
+  onSpacecraftSelected(spacecraft: Spacecraft, scenario: Scenario){
+    this.store.dispatch(SpacecraftActions.spacecraftSelected({spacecraft}))
+    this.store.dispatch(ScenariosActions.scenarioSelected({scenarioId: scenario.id}))
+
+    this.router.navigateByUrl(
+      `${scenario.name.trim()}-${spacecraft.catalogId.trim()}` || ''
+    );
   }
 
   onIconClick() {
@@ -81,20 +92,4 @@ export class ScenarioLibraryComponent {
     });
   }
 
-  ngOnInit() {
-    this.scenarios$.subscribe((res: any) => {
-      this.data = res.ids.map((id: string) => {
-        return res.entities[id];
-      });
-    });
-
-    this.spacecrafts$.subscribe((res: any) => {
-      this.spacecraftData = res;
-    });
-    this.store.dispatch(
-      SpacecraftActions.spacecraftSelected({
-        spacecraft: this.spacecraftData[0],
-      })
-    );
-  }
 }
