6416cd08a47472295a74436d319efc0037b118b0
Maria Paterno <97068764+Paterma@users.noreply.github.com>
2023-09-27 16:54:43 -0600

    chart is functional with data, trying to change colors and line cap

--- src/app/main/output-data-display/view-graph/view-graph.component.ts
+++ src/app/main/output-data-display/view-graph/view-graph.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   ApexAxisChartSeries,
@@ -12,6 +12,7 @@ import {
   ApexOptions,
   ChartComponent,
 } from 'ng-apexcharts';
+import { OutputDataDisplayService } from '../output-data-display.service';
 
 type ChartOptions = {
   series: ApexAxisChartSeries | any;
@@ -31,60 +32,51 @@ type ChartOptions = {
   templateUrl: './view-graph.component.html',
   styleUrls: ['./view-graph.component.css'],
 })
-export class ViewGraphComponent implements AfterViewInit {
+export class ViewGraphComponent {
   @ViewChild(ChartComponent) chart?: ChartComponent;
 
-  barChart = document.querySelector("#chart .apexcharts-bar-series .apexcharts[data-x='TEAM A']")?.classList.add('rangebar')
-  ngAfterViewInit(): void {
-    console.log(this.barChart, "barChart")
-    // if(this.chart) {
-    //   this.chart.updateSeries(this.chartOptions.series)
-    // }
-  }
+  constructor(public outputDataDisplayService: OutputDataDisplayService) {}
 
+  property = this.outputDataDisplayService.data.map((state) => state.property);
+
+  initialState = this.outputDataDisplayService.data.map(
+    (state) => state.initial
+  );
+
+  finalState = this.outputDataDisplayService.data.map((state) => state.final);
+
+  newDataArr = this.initialState.map((initial, index) => [
+    initial,
+    this.finalState[index],
+  ]);
+
+  barData = this.newDataArr.map((yData, index) => ({
+    x: index + ' data',
+    y: yData,
+    fill: 'red',
+    // fillColor: 'green',
+    colors: ['red', 'green'],
+    //  color: 'purple'
+  }));
 
   chartOptions: Partial<ChartOptions> | any = {
-    series: [
-      {
-        data: [
-          {
-            x: 'One',
-            y: [65, 96],
-          },
-          {
-            x: 'Two',
-            y: [55, 78],
-            colors: [
-              'var(--color-data-visualization-1)',
-              'var(--color-data-visualization-2)',
-              'var(--color-data-visualization-3)',
-              'var(--color-data-visualization-4)',
-            ],
-          },
-          {
-            x: 'Three',
-            y: [95, 186],
-            colors: [
-              'var(--color-data-visualization-4)',
-            ],
-          },
-        ],
-      },
-    ],
+    series: [{ data: this.barData }],
     plotOptions: {
       bar: {
         horizontal: false,
+        distributed: true,
+        background: '#293450',
         // style: {
-        //   cssClass: 'apexcharts-rangebar',
-        // },
+        colors: [
+          'var(--color-data-visualization-1)',
+          'var(--color-data-visualization-2)',
+          'var(--color-data-visualization-3)',
+          'var(--color-data-visualization-4)',
+        ],
+        // }
       },
     },
-    colors: [
-      'var(--color-data-visualization-1)',
-      'var(--color-data-visualization-2)',
-      'var(--color-data-visualization-3)',
-      'var(--color-data-visualization-4)',
-    ],
+
     chart: {
       height: 500,
       type: 'rangeBar',
@@ -95,11 +87,38 @@ export class ViewGraphComponent implements AfterViewInit {
         enabled: false,
       },
     },
+    colors: [
+      'var(--color-data-visualization-1)',
+      'var(--color-data-visualization-2)',
+      'var(--color-data-visualization-3)',
+      'var(--color-data-visualization-4)',
+    ],
+    // fill: {
+    //   colors: ['#F44336', '#E91E63', '#9C27B0'],
+    // },
+    fill: {
+      type: 'solid',
+      opacity: 0.6,
+      colors: ['#F44336', '#E91E63', '#9C27B0'],
+    },
+    stroke: {
+      lineCap: 'round',
+    },
+    //   dataLabels: {
+    //     style: {
+    //       colors: ['#F44336', '#E91E63', '#9C27B0']
+    //     }
+    //   },
+    //   markers: {
+    //     colors: ['#F44336', '#E91E63', '#9C27B0']
+    //  },
+    // theme: {
+    //   enabled: true
+    // },
     legend: {
       show: false,
     },
     xaxis: {
-      categories: [''],
       axisTicks: {
         show: true,
       },
