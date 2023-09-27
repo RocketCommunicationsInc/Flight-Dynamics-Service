e0e4f3a0474dc90f039c4cec8271a60747949ea9
Nic Dominguez <NicDominguez@users.noreply.github.com>
2023-09-27 16:08:48 -0700

    Refactor (for PR #40): implement tabbed-child-container (#44)
    * refactor: switch to tabbed child container
    
    * style: fix overflow of tables
    
    * chore: remove unused select function and clean up
    
    * style: fix table scroll

--- src/app/main/output-data-display/output-data-display.component.ts
+++ src/app/main/output-data-display/output-data-display.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AstroComponentsModule } from '@astrouxds/angular';
 import { SegmentedButton } from '@astrouxds/astro-web-components/dist/types/components';
 
 import type { Tabs } from 'src/app/types/Tabs';
-import { ChildContainerComponent } from 'src/app/shared';
+import { TabbedChildContainerComponent } from 'src/app/shared';
 import { SolutionGraphComponent } from './solution-graph/solution-graph.component';
 import { SolutionTableComponent } from './solution-table/solution-table.component';
 import { OutputDataDisplayService } from './output-data-display.service';
@@ -16,7 +16,7 @@ import { PerformanceTableComponent } from './performance-table/performance-table
   standalone: true,
   imports: [
     CommonModule,
-    ChildContainerComponent,
+    TabbedChildContainerComponent,
     AstroComponentsModule,
     SolutionGraphComponent,
     SolutionTableComponent,
@@ -26,9 +26,16 @@ import { PerformanceTableComponent } from './performance-table/performance-table
   styleUrls: ['./output-data-display.component.css'],
 })
 export class OutputDataDisplayComponent {
-  currentTab = 'od-solution';
-  currentView: CurrentView = 'View Table';
+  constructor(public outputDataDisplayService: OutputDataDisplayService) {}
 
+  currentView: CurrentView = 'View Table';
+  hasNotification = true;
+  notificationData = [
+    {
+      message: 'OD success message on 9/25/23, 10:20 AM',
+      status: 'normal',
+    },
+  ];
   tabs: Tabs[] = [
     { label: 'OD Solution', id: 'od-solution', selected: true },
     { label: 'OD Performance', id: 'od-performance' },
@@ -44,22 +51,17 @@ export class OutputDataDisplayComponent {
     { label: 'View Graph' },
   ];
 
-  constructor(public outputDataDisplayService: OutputDataDisplayService) {}
-
-  get isSolutionTab() {
-    return this.currentTab === 'od-solution';
-  }
-
-  get isPerformanceTab() {
-    return this.currentTab === 'od-performance';
-  }
-
   setCurrentView(e: Event) {
     const event = e as CustomEvent;
     this.currentView = event.detail;
   }
 
-  setCurrentTab(id: string) {
-    this.currentTab = id;
+  @HostListener('ruxselected', ['$event'])
+  onRuxSelected(e: any) {
+    if (e.detail.id === 'od-performance') {
+      this.hasNotification = false;
+    } else {
+      this.hasNotification = true;
+    }
   }
 }
