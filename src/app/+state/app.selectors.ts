7caf5a3f3354fab0e758d465f8a21d4b14bece44
Kiley <110477068+kiley-mitti@users.noreply.github.com>
2023-09-28 13:26:31 -0400

    feat(DEMO-382): add click handler sends to state (#45)
    * start changing names
    
    Co-authored-by: Frances Morrison <Frances.Morrison87@gmail.com>
    
    * fix: working on fixing route guards
    
    * feat: adding in spacecraft selector, using it on the spacecraft selection tree to add selected spacecraft into state, using it in utility toolkit to set spacecraft name.
    
    * route guard works..?
    
    Co-authored-by: Frances Morrison <Frances.Morrison87@gmail.com>
    
    * fix: refactoring selectedSpacecraft to selectedSpacecraftId to unpollute state, fixing state subscriptions to account
    
    * some changes
    
    * works, just messing with expand/collapse
    
    * states work
    
    * move all selectors to selectors
    
    * remove scenarioid subscription
    
    * css fix
    
    * refactor(DEMO-382 ): spacecraft selector refactor (#46)
    
    * refactor: creating a selector to get the current spacecraft, removing boilerplate in utility toolkit
    
    * chore: removing unsused code
    
    * nit: refactorin the selector
    
    * refactor: changing name of function and adding in track file selector
    
    * refactor: making async pipes work
    
    ---------
    
    Co-authored-by: Frances Morrison <francesrocket@Rocket-FRANCES.M>
    
    ---------
    
    Co-authored-by: Frances Morrison <Frances.Morrison87@gmail.com>
    Co-authored-by: Frances Morrison <francesrocket@Rocket-FRANCES.M>
    Co-authored-by: Frances Morrison <frances.morrison@rocketcom.com>

--- src/app/+state/app.selectors.ts
+++ src/app/+state/app.selectors.ts
@@ -1,13 +1,26 @@
 import { createSelector } from '@ngrx/store';
-import { Spacecraft } from '../types/data.types';
-import { ScenariosState } from './app.model';
-import { selectScenarios, selectTrackFiles } from './app.reducer';
+import { Spacecraft, TrackFile } from '../types/data.types';
+import { ScenariosState, TrackFilesState } from './app.model';
 import { scenarioAdapter, trackFileAdapter } from './app.adapters';
+import { appFeature } from './app.reducer';
+
+export const {
+  name,
+  reducer,
+  selectAppState,
+  selectScenarios,
+  selectTrackFiles,
+  selectSelectedSpacecraftId,
+  selectSelectedScenarioId,
+  selectSelectedTrackFileId,
+} = appFeature;
 
 export const { selectAll: selectAllScenarios } =
   scenarioAdapter.getSelectors(selectScenarios);
-export const { selectAll: selectAllTrackFiles } =
-  trackFileAdapter.getSelectors(selectTrackFiles);
+export const {
+  selectAll: selectAllTrackFiles,
+  selectEntities: selectTrackFileEntities,
+} = trackFileAdapter.getSelectors(selectTrackFiles);
 
 export const selectAllSpacecrafts = createSelector(
   selectScenarios,
@@ -17,5 +30,34 @@ export const selectAllSpacecrafts = createSelector(
       state.entities[id]?.spaceCraft.map((craft) => spacecrafts.push(craft));
     });
     return spacecrafts;
+  }
+);
+
+export const selectCurrentSpacecraft = createSelector(
+  selectAllSpacecrafts,
+  selectSelectedSpacecraftId,
+  (spacecrafts: Spacecraft[], spacecraftId: string | null) => {
+    if (!spacecraftId) return null;
+    return spacecrafts.find((craft) => craft.id === spacecraftId);
+  }
+);
+
+export const selectCurrentSpacecraftName = createSelector(
+  selectAllSpacecrafts,
+  selectSelectedSpacecraftId,
+  (spacecrafts: Spacecraft[], spacecraftId: string | null) => {
+    if (!spacecraftId) return null;
+    let spacecraft;
+    spacecraft = spacecrafts.find((craft) => craft.id === spacecraftId);
+    return spacecraft?.catalogId;
+  }
+);
+
+export const selectCurrentTrackFile = createSelector(
+  selectTrackFileEntities,
+  selectSelectedTrackFileId,
+  (trackFiles: any, trackFileId: string | null) => {
+    if (!trackFileId) return null;
+    return trackFiles[trackFileId];
   }
 );
