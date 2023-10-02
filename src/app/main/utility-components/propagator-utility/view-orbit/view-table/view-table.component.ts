946cb82ea30b0679d60999eb820500f426d115ff
Nic Dominguez <NicDominguez@users.noreply.github.com>
2023-10-02 14:18:20 -0700

    feat(DEMO-393): Propagator view orbit tab table view (#58)
    * feat: wip implementing table service
    
    * feat: implement table service
    
    * feat: implement pipe subscription

--- src/app/main/utility-components/propagator-utility/view-orbit/view-table/view-table.component.ts
+++ src/app/main/utility-components/propagator-utility/view-orbit/view-table/view-table.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AstroComponentsModule } from '@astrouxds/angular';
-
+import { ViewOrbitTableService } from '../view-orbit-table.service';
+import { TableService } from 'src/app/shared/table.service';
+import type { EphemerisFile } from 'src/app/types/data.types';
 @Component({
   selector: 'fds-view-table',
   standalone: true,
   imports: [CommonModule, AstroComponentsModule],
+  providers: [TableService],
   templateUrl: './view-table.component.html',
   styleUrls: ['./view-table.component.css'],
 })
-export class ViewTableComponent {}
+export class ViewTableComponent {
+  constructor(
+    public tableService: TableService<EphemerisFile>,
+    public viewOrbitTableService: ViewOrbitTableService
+  ) {
+    this.tableService.init({
+      columnDefs: this.viewOrbitTableService.columnDefs,
+      data: this.viewOrbitTableService.ephemerisData,
+    });
+  }
+}
