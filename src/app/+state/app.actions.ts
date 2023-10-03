68529cd2748eae65c35f1b26fe91d9165cd14ef9
Frances Morrison <frances.morrison@rocketcom.com>
2023-10-03 12:58:45 -0400

    feat(DEMO-326): Add state to input panel (#59)
    * refactor: setting selectors up using entities
    
    * move id initializers
    
    * feat: working on getting state into inputs
    
    * feat: track file state loaded into inputs component and displayed in form
    
    * chore: removing sync button and notification banner
    
    * feat: populating input form from store, populating database files, sending selected trackfile back to store.
    
    * feat: adding in trackfile processed for actions and reducer.
    
    * feat: pulling in generateProcessedTrackFile from mock data as a service, sing service to set a processed track file on a trackfile and sending it back into state
    
    * style: hacking the popup parts to make the popup look like a type-ahead dropdown
    
    * fix: spacing fix on first input dropdown
    
    * chore: ammending style to use tokens for dropdown
    
    * merge: resolving merge conflicts from main
    
    * refactor: trimming down unneeded code in scenario library
    
    * refactor: finalizing up requested changes, removing null declarations, removing orbit source select menu
    
    ---------
    
    Co-authored-by: Frances Morrison <francesrocket@Rocket-FRANCES.M>
    Co-authored-by: Kiley Mitti <kiley.mitti@rocketcom.com>

--- src/app/+state/app.actions.ts
+++ src/app/+state/app.actions.ts
@@ -1,5 +1,5 @@
 import { createActionGroup, emptyProps, props } from '@ngrx/store';
-import { Scenario, TrackFile } from '../types/data.types';
+import { ProcessedTrackFile, Scenario, TrackFile } from '../types/data.types';
 
 export const AppActions = createActionGroup({
   source: 'App',
@@ -30,6 +30,10 @@ export const TrackFilesActions = createActionGroup({
     'Track Files Requested': emptyProps(),
     'Track Files Retrieved': props<{ trackFiles: TrackFile[] }>(),
     'Track File Selected': props<{ trackFileId: string }>(),
+    'Track File Processed': props<{
+      processedTrackFile: ProcessedTrackFile;
+      trackFileId: string;
+    }>(),
     'Add Track File': props<{ trackFile: TrackFile }>(),
     'Remove Track File': props<{ trackFileId: string }>(),
   },
