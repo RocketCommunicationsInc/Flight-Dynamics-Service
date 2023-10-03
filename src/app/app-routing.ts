82907daa7822a6625b8c03dced1e9d5c4571a3fc
Brian <brian@briankiernan.com>
2023-10-03 17:54:53 -0400

    chore: remove unused components

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -1,8 +1,6 @@
 import { Route } from '@angular/router';
 import { LogUtilityComponent } from './main/utility-components/log-utility/log-utility.component';
 import { InputsOutputsDataDisplayComponent } from './main/inputs-outputs-data-display/inputs-outputs-data-display.component';
-import { CompareUtilityComponent } from './main/utility-components/compare-utility/compare-utility.component';
-import { CreateReportUtilityComponent } from './main/utility-components/create-report-utility/create-report-utility.component';
 import { TrackDataUtilityComponent } from './main/utility-components/track-data-utility/track-data-utility.component';
 import { PropagatorUtilityComponent } from './main/utility-components/propagator-utility/propagator-utility.component';
 import { validSpacecraftGuard } from './guards/valid-spacecraft.guard';
@@ -26,15 +24,7 @@ export const Routes: Route[] = [
         component: LogUtilityComponent,
       },
       {
-        path: 'compare',
-        component: CompareUtilityComponent,
-      },
-      {
-        path: 'report',
-        component: CreateReportUtilityComponent,
-      },
-      {
-        path: 'track',
+        path: 'track-data',
         component: TrackDataUtilityComponent,
       },
       {
