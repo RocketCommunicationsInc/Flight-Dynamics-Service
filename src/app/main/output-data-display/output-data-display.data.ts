f18f2907bcdfda5106e56c648bea1b479f0d5719
Maria Paterno <97068764+Paterma@users.noreply.github.com>
2023-10-04 15:24:40 -0600

    feature: total in cumulative table is now working

--- src/app/main/output-data-display/output-data-display.data.ts
+++ src/app/main/output-data-display/output-data-display.data.ts
@@ -19,40 +19,136 @@ const properties: Property[] = [
 
 export const PERFORMANCE_DATA = properties.map<PerformanceData>((property) => ({
   property,
-  finalAzimuth: randomNum(),
-  finalElevation: randomNum(1e6, 1e7),
-  finalRange: randomNum(),
-  initialAzimuth: randomNum(),
-  initialElevation: randomNum(1e6, 1e7),
-  initialRange: randomNum(),
+  finalAzimuth: randomNum(2,4),
+  finalElevation: randomNum(2,4),
+  finalRange: randomNum(2,4),
+  initialAzimuth: randomNum(2,4),
+  initialElevation: randomNum(2,4),
+  initialRange: randomNum(2,4),
 }));
 
-const cumulativeData = {
-  finalAzimuth: 0,
-  finalElevation: 0,
-  finalRange: 0,
-  initialAzimuth: 0,
-  initialElevation:0,
-  initialRange: 0,
-}
+export const PERFORMANCE_DATA_2 = properties.map<PerformanceData>((property) => ({
+  property,
+  finalAzimuth: randomNum(2,4),
+  finalElevation: randomNum(2,4),
+  finalRange: randomNum(2,4),
+  initialAzimuth: randomNum(2,4),
+  initialElevation: randomNum(2,4),
+  initialRange: randomNum(2,4),
+}));
 
-PERFORMANCE_DATA.map((property) => {
-  cumulativeData.finalAzimuth += property.finalAzimuth;
-  cumulativeData.finalElevation += property.finalElevation;
-  cumulativeData.finalRange += property.finalRange;
-  cumulativeData.initialAzimuth += property.initialAzimuth;
-  cumulativeData.initialElevation += property.initialElevation;
-  cumulativeData.initialRange += property.initialRange;
+export const PERFORMANCE_DATA_3 = properties.map<PerformanceData>((property) => ({
+  property,
+  finalAzimuth: randomNum(2,4),
+  finalElevation: randomNum(2,4),
+  finalRange: randomNum(2,4),
+  initialAzimuth: randomNum(2,4),
+  initialElevation: randomNum(2,4),
+  initialRange: randomNum(2,4),
+}));
+
+export const CUMULATIVE_DATA = properties.map((property: Property, index: number) => {
+  const columnSum = {
+    property,
+    finalAzimuth: 0,
+    finalElevation: 0,
+    finalRange: 0,
+    initialAzimuth: 0,
+    initialElevation:0,
+    initialRange: 0,
+  }
+  
+  columnSum.finalAzimuth = PERFORMANCE_DATA[index].finalAzimuth + PERFORMANCE_DATA_2[index].finalAzimuth + PERFORMANCE_DATA_3[index].finalAzimuth
+
+  columnSum.finalElevation = PERFORMANCE_DATA[index].finalElevation + PERFORMANCE_DATA_2[index].finalElevation + PERFORMANCE_DATA_3[index].finalElevation
+
+  columnSum.finalRange = PERFORMANCE_DATA[index].finalRange + PERFORMANCE_DATA_2[index].finalRange + PERFORMANCE_DATA_3[index].finalRange
+
+  columnSum.initialAzimuth = PERFORMANCE_DATA[index].initialAzimuth + PERFORMANCE_DATA_2[index].initialAzimuth + PERFORMANCE_DATA_3[index].initialAzimuth
+
+  columnSum.initialElevation = PERFORMANCE_DATA[index].initialElevation + PERFORMANCE_DATA_2[index].initialElevation + PERFORMANCE_DATA_3[index].initialElevation
+
+  columnSum.initialRange = PERFORMANCE_DATA[index].initialRange + PERFORMANCE_DATA_2[index].initialRange + PERFORMANCE_DATA_3[index].initialRange
+
+  return columnSum
 })
+
+// const cumulativeData = {
+//   finalAzimuth: [],
+//   finalElevation: [],
+//   finalRange: [],
+//   initialAzimuth: [],
+//   initialElevation:[],
+//   initialRange: [],
+// }
+
+// PERFORMANCE_DATA.map((property) => {
+//   cumulativeData.finalAzimuth += property.finalAzimuth;
+//   cumulativeData.finalElevation += property.finalElevation;
+//   cumulativeData.finalRange += property.finalRange;
+//   cumulativeData.initialAzimuth += property.initialAzimuth;
+//   cumulativeData.initialElevation += property.initialElevation;
+//   cumulativeData.initialRange += property.initialRange;
+// })
 
 //const total = Object.values()
 
-export const CUMULATIVE_DATA = properties.map<PerformanceData>((property) => ({
-  property,
-  finalAzimuth: randomNum(),
-  finalElevation: randomNum(1e6, 1e7),
-  finalRange: randomNum(),
-  initialAzimuth: randomNum(),
-  initialElevation: randomNum(1e6, 1e7),
-  initialRange: randomNum(),
-}));
+// const cumulativeDataArr: any[] = []
+
+// PERFORMANCE_DATA.map((property) => {
+//   const data = {
+//     property, 
+//     finalAzimuth: 0,
+//     finalElevation: 0,
+//     finalRange: 0,
+//     initialAzimuth: 0,
+//     initialElevation:0,
+//     initialRange: 0,
+//   }
+
+//   data.finalAzimuth = randomNum(2, 10)
+//   data.finalElevation = randomNum(2, 10)
+//   data.finalRange = randomNum(2, 10)
+//   data.initialAzimuth = randomNum(2, 10)
+//   data.initialElevation = randomNum(2, 10)
+//   data.initialRange = randomNum(2, 10)
+
+//   cumulativeDataArr.push(data)
+
+//   const columnSum = {
+//     finalAzimuth: cumulativeData.finalAzimuth.reduce((acc, val) => acc + val, 0),
+//     finalElevation: 0,
+//     finalRange: 0,
+//     initialAzimuth: 0,
+//     initialElevation:0,
+//     initialRange: 0,
+//   }
+
+//   const total = {
+//     properties, 
+//     ...columnSum
+//   }
+//   // for(const key in data) {
+//   //   if(key) {
+
+//   //     cumulativeData[key as keyof typeof cumulativeData].push(data[key])
+//   //   }
+//   // }
+
+//   cumulativeDataArr.push(total)
+// })
+
+// console.log(cumulativeDataArr)
+
+
+
+
+// export const CUMULATIVE_DATA = cumulativeDataArr.map<PerformanceData>((property) => ({
+//   property,
+//   finalAzimuth: randomNum(),
+//   finalElevation: randomNum(1e6, 1e7),
+//   finalRange: randomNum(),
+//   initialAzimuth: randomNum(),
+//   initialElevation: randomNum(1e6, 1e7),
+//   initialRange: randomNum(),
+// }));
