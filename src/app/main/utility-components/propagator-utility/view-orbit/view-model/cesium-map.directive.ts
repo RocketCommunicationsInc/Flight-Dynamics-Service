008bb238af05f891d1c80176931faaec2eddb050
Kiley <110477068+kiley-mitti@users.noreply.github.com>
2023-10-16 16:51:35 -0400

    feat(DEMO-424): realistic cesium orbit (#84)
    * cesium orbit
    
    * icon size
    
    * fix 3d zoom error
    
    * chore: comment out add points and add line code
    
    ---------
    
    Co-authored-by: Nic Dominguez <NicDominguez@users.noreply.github.com>

--- src/app/main/utility-components/propagator-utility/view-orbit/view-model/cesium-map.directive.ts
+++ src/app/main/utility-components/propagator-utility/view-orbit/view-model/cesium-map.directive.ts
@@ -4,24 +4,25 @@ import {
   EventEmitter,
   Input,
   OnChanges,
-  OnDestroy,
   Output,
   SimpleChanges,
 } from '@angular/core';
-import { OnInit } from '@angular/core';
 import {
-  Cartesian3,
-  Color,
+  // Cartesian3,
+  // Color,
   Viewer,
-  PolylineOutlineMaterialProperty,
+  // PolylineOutlineMaterialProperty,
   SceneMode,
+  CzmlDataSource,
+  // Cartesian2,
 } from 'cesium';
+import cesiumData from './cesium.json';
 
 @Directive({
   selector: '[fdsCesiumMap]',
   standalone: true,
 })
-export class CesiumMapDirective implements OnInit, OnChanges, OnDestroy {
+export class CesiumMapDirective {
   initialMetersFromEarth = 36_000_000;
   cameraChangeUnsubscribe: () => void = () => undefined;
   viewer: Viewer;
@@ -30,6 +31,7 @@ export class CesiumMapDirective implements OnInit, OnChanges, OnDestroy {
     this.viewer.scene.mode = SceneMode.SCENE2D;
     this.viewer.fullscreenButton.destroy();
     // this.viewer.camera.maximumZoomFactor = 1;
+    // this.viewer.camera.
   }
 
   @Input() zoomLevel: number = this.initialMetersFromEarth;
@@ -41,17 +43,21 @@ export class CesiumMapDirective implements OnInit, OnChanges, OnDestroy {
   @Output() onMetersFromEarthChange = new EventEmitter<number>();
 
   ngOnInit(): void {
-    this.viewer.camera.zoomOut(this.initialMetersFromEarth);
-    this.viewer.camera.flyTo({
-      destination: Cartesian3.fromDegrees(
-        this.satPos1X,
-        this.satPos1Y,
-        this.initialMetersFromEarth
-      ),
-    });
-    this.addPoint(this.satPos1X, this.satPos1Y);
-    this.addPoint(this.satPos2X, this.satPos2Y);
-    this.addLine(this.satPos1X, this.satPos1Y, this.satPos2X, this.satPos2Y);
+    // this.viewer.camera.zoomOut(this.initialMetersFromEarth);
+    // this.viewer.camera.flyTo({
+    //   destination: Cartesian3.fromDegrees(
+    //     this.satPos1X,
+    //     this.satPos1Y,
+    //     this.initialMetersFromEarth
+    //   ),
+    // });
+    // this.addPoint(this.satPos1X, this.satPos1Y);
+    // this.addPoint(this.satPos2X, this.satPos2Y);
+    // this.addLine(this.satPos1X, this.satPos1Y, this.satPos2X, this.satPos2Y);
+
+    this.viewer.dataSources.add(CzmlDataSource.load(cesiumData));
+
+    this.viewer.camera.flyHome(0);
 
     this.cameraChangeUnsubscribe = this.viewer.camera.changed.addEventListener(
       /**
@@ -68,69 +74,70 @@ export class CesiumMapDirective implements OnInit, OnChanges, OnDestroy {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
+    if (this.viewer.scene.mode !== SceneMode.SCENE2D) return;
     const zoomLevel = changes['zoomLevel'];
     const prevMetersFromEarth = zoomLevel.previousValue;
-    const currMetersFromEartch = zoomLevel.currentValue;
+    const currMetersFromEarth = zoomLevel.currentValue;
     if (zoomLevel.isFirstChange()) {
-      this.viewer.camera.zoomIn(currMetersFromEartch);
+      this.viewer.camera.zoomIn(currMetersFromEarth);
       return;
     }
-    if (currMetersFromEartch > prevMetersFromEarth) {
-      this.viewer.camera.zoomOut(currMetersFromEartch);
+    if (currMetersFromEarth > prevMetersFromEarth) {
+      this.viewer.camera.zoomOut(currMetersFromEarth);
       return;
     }
-    this.viewer.camera.zoomIn(currMetersFromEartch);
+    this.viewer.camera.zoomIn(currMetersFromEarth);
   }
 
   ngOnDestroy(): void {
     this.cameraChangeUnsubscribe();
   }
 
-  /**
-   * Creates and adds a point entity to the Cesium viewer.
-   *
-   * @param x the X degree of the point
-   * @param y the Y degree of the point
-   */
-  addPoint(x: number, y: number) {
-    this.viewer.entities.add({
-      position: Cartesian3.fromDegrees(x, y),
-      point: {
-        pixelSize: 5,
-        color: Color.fromCssColorString('#1b2d3e'),
-        outlineColor: Color.fromCssColorString('#00c7cb'),
-        outlineWidth: 2,
-      },
-    });
-  }
+  // /**
+  //  * Creates and adds a point entity to the Cesium viewer.
+  //  *
+  //  * @param x the X degree of the point
+  //  * @param y the Y degree of the point
+  //  */
+  // addPoint(x: number, y: number) {
+  //   this.viewer.entities.add({
+  //     position: Cartesian3.fromDegrees(x, y),
+  //     point: {
+  //       pixelSize: 5,
+  //       color: Color.fromCssColorString('#1b2d3e'),
+  //       outlineColor: Color.fromCssColorString('#00c7cb'),
+  //       outlineWidth: 2,
+  //     },
+  //   });
+  // }
 
-  /**
-   * Draws a line between the two given points.
-   *
-   * @param x1 satPos1X - the X degree of the first point
-   * @param y1 satPos1Y - the Y degree of the first point
-   * @param x2 satPos2X - the X degree of the second point
-   * @param y2 satPos2Y - the Y degree of the second point
-   */
-  addLine(x1: number, y1: number, x2: number, y2: number) {
-    this.viewer.entities.add({
-      name: this.name,
-      polyline: {
-        positions: Cartesian3.fromDegreesArrayHeights([
-          x1,
-          y1,
-          250000,
-          x2,
-          y2,
-          250000,
-        ]),
-        width: 5,
-        material: new PolylineOutlineMaterialProperty({
-          color: Color.fromCssColorString('#00c7cb'),
-          outlineWidth: 2,
-          outlineColor: Color.BLACK,
-        }),
-      },
-    });
-  }
+  // /**
+  //  * Draws a line between the two given points.
+  //  *
+  //  * @param x1 satPos1X - the X degree of the first point
+  //  * @param y1 satPos1Y - the Y degree of the first point
+  //  * @param x2 satPos2X - the X degree of the second point
+  //  * @param y2 satPos2Y - the Y degree of the second point
+  //  */
+  // addLine(x1: number, y1: number, x2: number, y2: number) {
+  //   this.viewer.entities.add({
+  //     name: this.name,
+  //     polyline: {
+  //       positions: Cartesian3.fromDegreesArrayHeights([
+  //         x1,
+  //         y1,
+  //         250000,
+  //         x2,
+  //         y2,
+  //         250000,
+  //       ]),
+  //       width: 5,
+  //       material: new PolylineOutlineMaterialProperty({
+  //         color: Color.fromCssColorString('#00c7cb'),
+  //         outlineWidth: 2,
+  //         outlineColor: Color.BLACK,
+  //       }),
+  //     },
+  //   });
+  // }
 }
